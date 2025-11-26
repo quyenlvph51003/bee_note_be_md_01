2e7c2b74d749d2c1b72c0f7763697fd783de54bb
Tên của bạn <Email_ng_k_GitLab@example.com>
2025-11-26 14:04:11 +0700

    Gửi Mã OTP

---
+++ utils/sendMail.js
@@ -0,0 +1,21 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+async function sendMail(to, subject, text) {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,  // Gmail bạn dùng để login SMTP
+      pass: process.env.EMAIL_PASS,  // Mật khẩu ứng dụng 16 ký tự
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"Bee Note" <${process.env.EMAIL_USER}>`, // phải trùng với user
+    to,
+    subject,
+    text,
+  });
+}
+
+module.exports = { sendMail };
